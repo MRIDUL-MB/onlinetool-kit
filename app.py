d7677aeeb63f194a8655833e31f05232c809c243
mridul-mb <mriravbagla@gmail.com>
2023-03-18 19:47:39 +0530

    changing-2 Dockerfile

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ def download_video():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3001)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 2343)), debug=True)
